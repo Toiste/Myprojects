3fbd8f6208664c6039dd9bbb838e5d748b773a87
Marllon Ribeiro <Marlinn.bb@gmail.com>
2022-01-07 19:07:24 -0300

    Cronometro Pronto
    Um cronometro que conta os segundos/ minutos, com função de começar, parar e resetar.

--- Cronometro/script.js
+++ Cronometro/script.js
@@ -1,8 +1,43 @@
+let minutos = document.getElementById("minutos")
+let segundos = document.getElementById("segundos")
+let segundosdez = document.getElementById("segundosdez")
+let IntervalID;
+let seg = 0
+let min = 0
+let segdez = 0
+
 function comecar() {
-}
-function parar() {
+IntervalID = setInterval(function(){
+    console.log(`${seg}`)
+        seg += 1
+        segundos.innerHTML = `${seg}`
+    
+    if (seg == 10){
+        segundosdez.innerHTML = ""
+    }
+
+    if (seg == 60){
+        min += 1
+        minutos.innerHTML = `${min}`
+        seg = 0
+        segundos.innerHTML = `0`
+        segundosdez.innerHTML = `0`
+    }
+    
+    }, 1000);
+
 
 }
+
+function parar() {
+    clearInterval(IntervalID)
+}
+
 function resetar() {
+    clearInterval(IntervalID)
+    seg = 0
+    segundos.innerHTML = "0"
+    segundosdez.innerHTML = "0"
+    minutos.innerHTML = "0"
 
 }
