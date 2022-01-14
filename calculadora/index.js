422fb7e1d11e9eb91d40c10957a5579a33b3419d
Marllon Ribeiro <Marlinn.bb@gmail.com>
2022-01-14 17:46:48 -0300

    Melhorias na calculadora
    Nesse novo commit eu fiz melhorias no script.
     Agora você n pode começar usando um operador ou o ponto
     não pode repeti-lo e nem usa-lo após o ponto.
     O asterisco que antes aparecia na calculadora foi trocado pelo x tradicional.
     O calculo é feito em uma variável separada e apresentada na string.

--- calculadora/index.js
+++ calculadora/index.js
@@ -1,5 +1,64 @@
+var resultado = ''
+const operators = ['+', '-', '*', '/', '.'];
 
-function adicionar(a) {
+
+
+/* Adiciona o X ao html e o (*) a (var resultado) */
+function addmultiplicador(d) {
+    if (resultado.length != 0){
+        if (operators.includes(resultado[resultado.length - 1])){
+        } else {
+            if (resultado.length != 0){
+                document.getElementById('res').innerHTML += 'x'
+                resultado += d
+                console.log(resultado)
+            }
+        }
+    }
+}
+
+// Adiciona os operadores e o (.)
+function addoperador(c) {
+        if (resultado.length != 0){
+            if (operators.includes(resultado[resultado.length - 1])){
+            
+            } else {
+                document.getElementById('res').innerHTML +=c
+                resultado += c
+            }
+        }
+}
+
+// Adiciona os numeros
+function addnumber(a) {
+    document.getElementById('res').innerHTML +=a
+    resultado += a
+    console.log(resultado)
+}
+
+//Limpa o HTML e o (var resultado)
+function limpar(b) {
+    document.getElementById('res').innerHTML = b;
+    resultado = b
+    console.log(resultado)
+}
+
+//usa eval pra calcular a string de (var resultado) e joga no HTML
+function calcular() {
+    let res = document.getElementById('res').innerHTML;
+    if (res) {
+        document.getElementById('res').innerHTML = eval(resultado)
+    }
+}
+
+
+
+
+
+
+      /* PRIMEIRA VERSAO DO JS DA CALCULADORA*/
+
+/*function adicionar(a) {
     document.getElementById('res').innerHTML += a;
 }
 
@@ -11,5 +70,5 @@ function calcular() {
     if (res) {
         document.getElementById('res').innerHTML = eval(res)
     }
-}
+}*/
 
