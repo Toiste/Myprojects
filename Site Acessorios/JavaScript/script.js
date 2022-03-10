4642a49a271e4c439d73921e4168fdfbcc53171a
Marllon Ribeiro <Marlinn.bb@gmail.com>
2022-03-09 22:30:47 -0300

    transition

--- Site Acessorios/JavaScript/script.js
+++ Site Acessorios/JavaScript/script.js
@@ -1,17 +1,18 @@
 const produtos = document.querySelector("#produtos");
 const menu = document.querySelector("#menu");
-
-const distanceTopProdutos = produtos.getBoundingClientRect().top;
+const informationBar = document.querySelector(".information-bar")
 menu.classList.add("main-menu")
 
  window.addEventListener("scroll", () => {
     console.log(window.scrollY)
-     if (window.scrollY >= 50) {
-        menu.classList.remove("main-menu")
-        menu.classList.add("menu-white")
-     } else {
-        menu.classList.add("main-menu")
-        menu.classList.remove("menu-white")
-     }
+   if (window.scrollY >= 50) {
+      informationBar.style.display = "none";
+      menu.classList.remove("main-menu")
+      menu.classList.add("menu-white")
+   } else {
+      informationBar.style.display = "flex";
+      menu.classList.add("main-menu")
+      menu.classList.remove("menu-white")
+   }
  })
 
