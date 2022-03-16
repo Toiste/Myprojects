a939ac4324f313bac0886259f061ef5c51f2a5df
Marllon Ribeiro <Marlinn.bb@gmail.com>
2022-03-16 20:08:52 -0300

    media query 1024px

--- Site Acessorios/JavaScript/script.js
+++ Site Acessorios/JavaScript/script.js
@@ -5,14 +5,18 @@ menu.classList.add("main-menu")
 
  window.addEventListener("scroll", () => {
     console.log(window.scrollY)
-   if (window.scrollY >= 80) {
+    if (window.matchMedia("(max-width: 1024px)").matches) {
       informationBar.style.display = "none";
-      menu.classList.remove("main-menu")
-      menu.classList.add("menu-white")
-   } else {
-      informationBar.style.display = "flex";
-      menu.classList.add("main-menu")
-      menu.classList.remove("menu-white")
-   }
+    } else {
+      if (window.scrollY >= 80) {
+         informationBar.style.display = "none";
+         menu.classList.remove("main-menu")
+         menu.classList.add("menu-white")
+      } else {
+         informationBar.style.display = "flex";
+         menu.classList.add("main-menu")
+         menu.classList.remove("menu-white")
+      } 
+    }
  })
 
