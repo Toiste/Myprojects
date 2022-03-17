99a27298e8a19e0f421b9632de47df28f6110ca7
Marllon Ribeiro <Marlinn.bb@gmail.com>
2022-03-17 15:34:42 -0300

    problemas no menu
    ao diminuir a tela o menu entrava em conflito, por causa do eventlistener "scroll". Mas foi "resolvido", colocando o main-menu direto no html... como foi resolvido?, não sei.

--- Site Acessorios/JavaScript/script.js
+++ Site Acessorios/JavaScript/script.js
@@ -1,22 +1,17 @@
 const produtos = document.querySelector("#produtos");
 const menu = document.querySelector("#menu");
 const informationBar = document.querySelector(".information-bar")
-menu.classList.add("main-menu")
 
  window.addEventListener("scroll", () => {
     console.log(window.scrollY)
-    if (window.matchMedia("(max-width: 1024px)").matches) {
-      informationBar.style.display = "none";
-    } else {
       if (window.scrollY >= 80) {
          informationBar.style.display = "none";
-         menu.classList.remove("main-menu")
          menu.classList.add("menu-white")
+
       } else {
          informationBar.style.display = "flex";
-         menu.classList.add("main-menu")
          menu.classList.remove("menu-white")
+         
       } 
-    }
- })
+    })
 
