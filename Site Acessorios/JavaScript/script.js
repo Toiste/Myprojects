948f422c37998d175cd16e783f90b5664fea9b28
Marllon Ribeiro <Marlinn.bb@gmail.com>
2022-03-18 01:55:44 -0300

    problemas com menu resolvidos.
    conflito com o main menu e o menu white. uso do resize e do matchmedia pra ajustes.

--- Site Acessorios/JavaScript/script.js
+++ Site Acessorios/JavaScript/script.js
@@ -2,16 +2,27 @@ const produtos = document.querySelector("#produtos");
 const menu = document.querySelector("#menu");
 const informationBar = document.querySelector(".information-bar")
 
- window.addEventListener("scroll", () => {
-    console.log(window.scrollY)
-      if (window.scrollY >= 80) {
-         informationBar.style.display = "none";
-         menu.classList.add("menu-white")
+const addWhite = () => {
 
-      } else {
-         informationBar.style.display = "flex";
-         menu.classList.remove("menu-white")
-         
-      } 
-    })
+   console.log(window.scrollY)
+     if (window.scrollY >= 80) {
+        informationBar.style.display = "none";
+        menu.classList.add("menu-white")
 
+     } else {
+        informationBar.style.display = "flex";
+        menu.classList.remove("menu-white")
+        
+     } 
+   }
+
+ window.addEventListener("scroll", addWhite)
+
+window.addEventListener("resize", () => {
+   if (window.matchMedia("(max-width: 1024px)").matches) {
+      window.removeEventListener("scroll", addWhite)
+      menu.classList.remove("menu-white")
+    } else {
+      window.addEventListener("scroll", addWhite)
+    }
+})
